dc895c895789703599cecf87d191094184fd51a4
= <=>
2022-08-24 21:16:41 -0400

    Rota Post

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const checkListRouter = require('./src/routes/checklist');
 
+
+const app = express();
+app.use(express.json());
+app.use('/checklist', checkListRouter);
+
+app.listen(3000, ()=> {
+    console.log('Servidor Foi Iniciado');
+});
